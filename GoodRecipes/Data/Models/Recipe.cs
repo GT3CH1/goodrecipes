079302e9f2c4c2f19bc4062bcd61c722aeaa2b06
GT3CH1 <gavinpease@gmail.com>
2023-05-27 15:20:39 -0600

    Add Initial recipe based objects

---
+++ GoodRecipes/Data/Models/Recipe.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2022. Gavin Pease and contributors.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial
+// portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+// OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GoodRecipes.Data.Models;
+
+public class Recipe
+{
+
+    [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id;
+    
+    [Required] public string Name { get; set; }
+
+    [Required] public string ThumbnailUrl { get; set; }
+
+    public Guid RecipeAuthor;
+
+    [Required]
+    [ForeignKey("RecipeAuthor")]
+    public RecipeUser Author { get; set; }
+
+    [Required] public List<Instructions> InstructionsList { get; set; }
+
+    public string? Story { get; set; } = null!;
+
+    public List<Tags> TagsList { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,80 @@
+﻿# Good Recipes
+
+A project by Alex Bennett (@ajbenn90) and Gavin Pease (@GT3CH1)
+
+### Database ToDo's
+
+#### User
+
+- [ ] Email
+    - Unique, not null
+- [ ] Username
+    - Unique, not null
+- [ ] Suspended
+    - Boolean, not null
+- [ ] SuspendEndDate
+    - Date, nullable
+- [ ] Food Preferences
+    - Array of strings, nullable
+- [ ] Recipes
+    - Array of Recipe IDs, nullable
+- [ ] Favorite Recipes
+    - Array of Recipe IDs, nullable
+- [ ] Reviews
+    - List of comments to recipe IDs
+- [ ] Following Users
+    - List of User ID's
+- [ ] Comment/Post reports.
+
+#### Recipe
+
+- [ ] Name
+- [ ] Thumbnail
+- [ ] Author
+- [ ] Sections
+- [ ] Instructions
+- [ ] Story
+- [ ] Tags
+
+#### Ingredient
+
+- [ ] Name
+- [ ] Categories
+    - Such as animal-product, nut
+
+#### Ingredient Categories
+
+- [ ] List of different food categories.
+
+#### Review
+
+- [ ] Rating
+- [ ] Text
+- [ ] Recipe
+- [ ] Author
+- [ ] Helpful votes
+
+#### Reports
+
+- [ ] Type
+- [ ] Reporter User
+- [ ] Reported User
+- [ ] Reported Content
+- [ ] Reviewed
+- [ ] Accepted
+- [ ] Reviewed By
+- [ ] Reviewed date
+
+#### Recipe Ingredients
+
+- [ ] Quantity
+- [ ] Recipe
+- [ ] Ingredient
+
+Q: How do we want to represent this?
+
+#### Review Vote
+
+- [ ] Review
+- [ ] User
+- [ ] Helpfulness
