b896f983b51d52410732ec956852c354e3bc3120
GT3CH1 <gavinpease@gmail.com>
2023-05-27 13:29:20 -0600

    Working webpage!

--- GoodRecipes/Program.cs
+++ GoodRecipes/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+// add IndexController
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Index}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
