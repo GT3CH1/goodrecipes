f546a230c2072095f51e9e60db08841bcb029745
GT3CH1 <gavinpease@gmail.com>
2023-06-29 09:09:50 -0600

    Add scaffolded Recipe

---
+++ GoodRecipes/Controllers/RecipeController.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GoodRecipes.Data;
+using GoodRecipes.Models;
+
+namespace GoodRecipes
+{
+    public class RecipeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecipeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Recipe
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Recipes.Include(r => r.Author);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Recipe/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Recipes == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipes
+                .Include(r => r.Author)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+
+        // GET: Recipe/Create
+        public IActionResult Create()
+        {
+            ViewData["Id"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Recipe/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,ThumbnailPath,InstructionsPath,StoryPath,TimeCreated")] Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recipe);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Id"] = new SelectList(_context.Users, "Id", "Id", recipe.Id);
+            return View(recipe);
+        }
+
+        // GET: Recipe/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Recipes == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            ViewData["Id"] = new SelectList(_context.Users, "Id", "Id", recipe.Id);
+            return View(recipe);
+        }
+
+        // POST: Recipe/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Description,ThumbnailPath,InstructionsPath,StoryPath,TimeCreated")] Recipe recipe)
+        {
+            if (id != recipe.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recipe);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecipeExists(recipe.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Id"] = new SelectList(_context.Users, "Id", "Id", recipe.Id);
+            return View(recipe);
+        }
+
+        // GET: Recipe/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Recipes == null)
+            {
+                return NotFound();
+            }
+
+            var recipe = await _context.Recipes
+                .Include(r => r.Author)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipe);
+        }
+
+        // POST: Recipe/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Recipes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Recipes'  is null.");
+            }
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe != null)
+            {
+                _context.Recipes.Remove(recipe);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecipeExists(string id)
+        {
+          return (_context.Recipes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
